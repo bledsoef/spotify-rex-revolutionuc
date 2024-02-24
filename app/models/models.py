4bea86d640f2d5853a4400281f610e3e9f9d359d
bledsoef <bledsoef@berea.edu>
2024-02-24 15:50:28 -0500

    getting recs for users

--- app/models/models.py
+++ app/models/models.py
@@ -11,9 +11,23 @@ class User(Base):
     email = Column(String(50))
 
     created_recs = relationship("Rec", back_populates="creator", foreign_keys="[Rec.createdBy]")
+    created_reviews = relationship("Review", back_populates="creator", foreign_keys="[Review.createdBy]")
 
     # Define relationship with Rec for the received recommendations
     received_recs = relationship("Rec", back_populates="recipient", foreign_keys="[Rec.sentTo]")
+
+class Review(Base):
+    __tablename__ = "reviews"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    createdBy = Column(String(50), ForeignKey("users.username"))
+    rec_id = Column(Integer, ForeignKey("recs.id"), nullable=False)  # Change data type to INTEGER
+    dateCreated = Column(Date, nullable=False)
+    comment = Column(String(50), nullable=True)
+    rating = Column(Integer, nullable=True)
+
+    creator = relationship("User", back_populates="created_reviews", foreign_keys=[createdBy])
+    rec_obj = relationship("Rec", back_populates="reviews", foreign_keys=[rec_id])
 
 class Rec(Base):
     __tablename__ = "recs"
@@ -23,8 +37,14 @@ class Rec(Base):
     createdBy = Column(String(50), ForeignKey("users.username"))
     sentTo = Column(String(50), ForeignKey("users.username"), nullable=True)
     isPost = Column(Boolean, nullable=False)
+    # completed, pending, accepted, rejected
+    status = Column(String(50), nullable=False)
 
     creator = relationship("User", back_populates="created_recs", foreign_keys=[createdBy])
 
     # Define relationship with User for the recipient
     recipient = relationship("User", back_populates="received_recs", foreign_keys=[sentTo])
+
+    # Explicitly define the join condition for the reviews relationship
+    reviews = relationship("Review", back_populates="rec_obj", foreign_keys=[Review.rec_id],
+                           primaryjoin="Rec.id == Review.rec_id")
